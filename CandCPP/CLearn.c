e2bf7850ebf9e5dc0539c4d18b155f43f5573c91
yu wangyu <695182937@qq.com>
2025-10-29 19:23:50 +0800

    10-29

---
+++ CandCPP/CLearn.c
@@ -0,0 +1,60 @@
+//C语言学习知识点
+//配套自学网站：https://xiecoding.cn/
+//配套视频网站：https://study.xiaoe-tech.com/
+
+//1-基础C语言语法知识
+
+
+int main()  //main 表示一个主函数
+{
+	// 主函数返回值  注释：标注代码用途和思路
+	return 0; //return：表示函数返回值
+}
+
+/*
+ 返回值类型 函数名（输入参数值）
+ {
+	做点什么事
+	return 函数返回值
+ }
+ */
+
+//所有关键字
+
+//auto      _Bool      break     case
+//char      _Complex   const     continue
+//default   restrict   do        double
+//else      enum       extern    float
+//for       goto       if        Imaginary
+//inline    int        long      register
+//return    short      signed    sizeof
+//static    struct     switch    typedef
+//union     unsigned   void      volatile
+//while
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ DataStructures_Aigorithms/DataStructures_Algorithm.c
@@ -0,0 +1,2 @@
+//数据结构与算法
+//学习网址：https://xiecoding.cn/
